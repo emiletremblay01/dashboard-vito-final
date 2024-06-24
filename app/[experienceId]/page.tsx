c1a846a845e4a0686e477c5113a9f6dae732b080
Emile Tremblay <76021392+emiletremblay01@users.noreply.github.com>
2024-06-24 14:57:43 -0400

    wip

--- app/[experienceId]/page.tsx
+++ app/[experienceId]/page.tsx
@@ -1,6 +1,22 @@
 import { AddExperienceForm } from "@/components/form-add-experience";
 
-export default function AddExperiencePage() {
+export default async function EditExperiencePage({
+  params,
+}: {
+  params: { experienceId: string; };
+}) {
+  const experience = await getExperienceById(experienceId);
+  
+  if (!experience) {
+    return (
+    <div className="container flex gap-6 p-2 pt-16">
+      <div className="mx-auto w-full max-w-sm">
+        <h2 className="text-2xl font-semibold">No experience found</h2>
+      </div>
+    </div>
+  );
+  }
+  
   return (
     <div className="container flex gap-6 p-2 pt-16">
       <div className="mx-auto w-full max-w-sm">
