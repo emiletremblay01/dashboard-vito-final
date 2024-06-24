855c49d58dbe29790fd937eef35d5444fa4b80a5
Emile Tremblay <76021392+emiletremblay01@users.noreply.github.com>
2024-06-24 15:00:40 -0400

    wip

--- data/experience.ts
+++ data/experience.ts
@@ -3,3 +3,11 @@ import prismadb from "@/lib/prismadb";
 export const getAllExperiences = async () => {
   return prismadb.experience.findMany();
 };
+
+export const getExperienceById = async (id: string) => {
+  return prismadb.experience.findUnique({
+    where: {
+      id,
+    },
+  });
+};
