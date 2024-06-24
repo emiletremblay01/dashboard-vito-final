d441ba4bb48ae1afeecbe49b2cf3f2f9612b69e3
Emile Tremblay <76021392+emiletremblay01@users.noreply.github.com>
2024-06-24 15:06:46 -0400

    fixed it

--- app/[experienceId]/page.tsx
+++ app/[experienceId]/page.tsx
@@ -5,7 +5,7 @@ export default async function EditExperiencePage({
 }: {
   params: { experienceId: string; };
 }) {
-  const experience = await getExperienceById(experienceId);
+  const experience = await getExperienceById(params.experienceId);
   
   if (!experience) {
     return (
